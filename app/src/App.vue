7e76293c0c8ad217b5ae43dc41510cb712895907
aelmore49 <alexelmore49@gmail.com>
2022-11-23 08:52:46 -0600

    setup header component and add icon for header

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+  <div class="container">start</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+};
 </script>
 
 <style>
---
+++ app/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="text-center bg-green-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl mb-3">
+      <i class="fa-solid fa-otter"></i>
+      National Park's App
+    </div>
+    <p>
+      API by
+      <a
+        class="text-yellow-300"
+        href="https://www.nps.gov/subjects/developer/api-documentation.htm"
+        target="_blank"
+        >The National Park Service</a
+      >
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
