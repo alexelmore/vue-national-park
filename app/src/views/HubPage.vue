5efe1443a933266a054cfa1027b04815d787ea11
aelmore49 <alexelmore49@gmail.com>
2022-11-24 10:48:08 -0600

    setup for ParkList component

--- app/src/views/HubPage.vue
+++ app/src/views/HubPage.vue
@@ -1,18 +1,24 @@
 <template>
   <div v-if="loading"><BaseSpinner /></div>
-  <div v-else>
-    <div v-for="park in parks" :key="park.id">
-      <h2>{{ park.fullName }}</h2>
+  <div class="flex flex-col align-center justify-center text center" v-else>
+    <div
+      class="text-gray-500 text-3xl mt-10 mb-6"
+      v-for="park in parks"
+      :key="park.id"
+    >
+      <ParkList :park="park" />
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-
+import ParkList from "./parks/ParkList.vue";
 export default {
   name: "HubPage",
-
+  components: {
+    ParkList,
+  },
   props: {
     loading: {
       type: Boolean,
---
+++ app/src/views/parks/ParkList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 class="text-3xl font-bold">{{ park.fullName }}</h2>
+    <img
+      class="w-72 m-auto mt-10"
+      :src="park.images[0].url"
+      :alt="park.images[0].alt"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ParkList",
+  props: {
+    park: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
