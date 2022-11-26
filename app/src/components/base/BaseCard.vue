d1f5ec5148df15249c74688d6406f603573f5578
aelmore49 <alexelmore49@gmail.com>
2022-11-26 10:28:35 -0600

    card styling

--- app/src/components/base/BaseCard.vue
+++ app/src/components/base/BaseCard.vue
@@ -1,9 +1,7 @@
 
 
 <template>
-  <div
-    class="bg-green-800 w-96 rounded-xl shadow-2xl p-6 mx-auto mt-8 max-w-2xl"
-  >
+  <div class="card bg-green-800 w-96 rounded-xl p-6 mx-auto mt-8 max-w-2xl">
     <slot></slot>
   </div>
 </template>
@@ -16,10 +14,15 @@ export default {
 
 <style scoped>
 .card {
-  border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-  margin: 2rem auto;
-  max-width: 40rem;
+  cursor: pointer;
+  transition: transform 0.3s, filter 1.5s ease-in-out;
+  filter: grayscale(100%);
+  transform: scale(1);
+}
+
+.card:hover {
+  filter: grayscale(0);
+  transform: scale(1.1);
 }
 </style>
